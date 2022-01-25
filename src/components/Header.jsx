226959d2bcaaa7757e3c588d36c3eda1133c962d
saifullah rahman <saiflll284@gmail.com>
2022-01-26 04:08:04 +0530

    added fade animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-function Header({text}) {
+function Header({ text }) {
   return (
-      <div className='navbar mb-12 shadow-lg flex justify-center bg-[#1c1b2498]'>
-          <h2 className='text-3xl'>{text}</h2>
-      </div>
-  )
+    <div className="navbar mb-12 shadow-lg flex justify-center bg-[#1c1b2498]">
+      <h2 className="text-3xl">
+        <a href="/">{text}</a>
+      </h2>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,39 +2,64 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import { useContext, useEffect, useState } from "react";
 import TodosContext from "../context/TodosContext";
+import { motion, AnimatePresence } from "framer-motion";
 
 function TodoList() {
   const { todos } = useContext(TodosContext);
   const [completedTodos, setCompletedTodos] = useState([]);
-  const [notCompletedTods, setNotCompletedTods] = useState([]);
+  const [notCompletedTodos, setNotCompletedTodos] = useState([]);
 
   useEffect(() => {
     setCompletedTodos(todos.filter((todo) => todo.done));
-    setNotCompletedTods(todos.filter((todo) => !todo.done));
+    setNotCompletedTodos(todos.filter((todo) => !todo.done));
   }, [todos]);
 
   return (
     <>
       <div className="container rounded-3xl shadow-lg w-100 md-w-75 lg:w-1/2 xl:w-1/3 m-auto">
-        {notCompletedTods.map((todo) => (
-          <TodoItem todo={todo} key={todo.id} />
-        ))}
+        <AnimatePresence>
+          {notCompletedTodos.length === 0 ? (
+            <p className="container m-3">Nothing Here</p>
+          ) : (
+            notCompletedTodos.map((todo) => (
+              <motion.div
+                key={todo.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <TodoItem todo={todo} key={todo.id} />
+              </motion.div>
+            ))
+          )}
 
-        {completedTodos.length === 0 ? null : (
-          <>
-            <div className="collapse w-100 border border-base-300 collapse-arrow">
-              <input type="checkbox" />
-              <div className="collapse-title text-xl font-medium">
-                Completed{" "}
+          {completedTodos.length === 0 ? null : (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <div className="collapse w-100 border border-base-300 collapse-arrow">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                  Completed{" "}
+                </div>
+                <div className="collapse-content">
+                  {completedTodos.map((todo) => (
+                    <motion.div
+                      key={todo.id}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                    >
+                      <TodoItem todo={todo} key={todo.id} />
+                    </motion.div>
+                  ))}
+                </div>
               </div>
-              <div className="collapse-content">
-                {completedTodos.map((todo) => (
-                  <TodoItem todo={todo} key={todo.id} />
-                ))}
-              </div>
-            </div>
-          </>
-        )}
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
